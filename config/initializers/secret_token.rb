c7b05cba2eb7584ca82ddb5a05f4607ba2f68f44
Winston W <winstwu@gmail.com>
2013-05-20 15:01:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RsgLinkedinGem::Application.config.secret_token = '8ce64fcc4833bdd73159e2ece18c557d33091df5f207d63fcb005c1a9a45f9845d486261fead008655e5d0fb4a303df5ae8be2d7cb02feebdf045bf9fb994f47'
